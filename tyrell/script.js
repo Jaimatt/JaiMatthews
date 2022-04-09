e51d22bc6a2f7d487d7b25ef646d755d5207b2fe
Jaimatt <79958948+Jaimatt@users.noreply.github.com>
2022-04-09 20:14:25 +0930

    Add files via upload

---
+++ tyrell/script.js
@@ -0,0 +1,5 @@
+function change_site(){
+    window.open("https://www.youtube.com/watch?v=-AXetJvTfU0", '_blank').focus();
+    setTimeout(document.getElementById("expansion").style.display = "block", 1000);
+    document.getElementById("expansion").style.display = "block"
+}
